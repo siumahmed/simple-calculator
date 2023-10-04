239d5891e533e6a624421b518f898d48979a6a91
SIAM THE DEV <130205459+siumahmed@users.noreply.github.com>
2023-10-04 14:54:55 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+let displayValue = '';
+
+function appendToDisplay(value){
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+function clearDisplay(){
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function backspace(){
+    displayValue = displayValue.slice(0, -1);
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult(){
+    try{
+        displayValue = eval(displayValue).toString();
+        document.getElementById('display').value = displayValue;
+    }
+    catch(error){
+        displayValue = 'Error';
+        document.getElementById('display').value = 'Error';
+    }
+};
+
